d466deff2c5a2bdf84b712a03524c810fb30429b
prince prasad <pp1614582@gmail.com>
2025-04-23 02:22:00 +0530

    commit 2

--- src/components/Child1.jsx
+++ src/components/Child1.jsx
@@ -1,11 +1,11 @@
 
 import React from "react";
-function Child1(handleClick) {
+function Child1({ handleClick }) {
     return (
         <div>
             <h3>Child Component 1</h3>
             <button
-                onClick={() => handleClick("Option 1")}
+                onClick={() => { handleClick("Option 1") }}
             >Option 1</button>
         </div>
     )
